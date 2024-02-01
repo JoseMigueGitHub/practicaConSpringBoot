cbd39340ba621549dc66e6be46218f5b4482e9dd
JoseM <pcasjosemiguel@gmail.com>
2024-01-31 21:01:31 -0600

    se descarga proyecto spring

---
+++ spring-boot-wed/spring-boot-wed/src/main/java/com/bolsadeideas/springboot/app/springbootwed/app/SpringBootWedApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app.springbootwed.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWedApplication.class, args);
+	}
+
+}
---
+++ spring-boot-wed/spring-boot-wed/src/test/java/com/bolsadeideas/springboot/app/springbootwed/app/SpringBootWedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app.springbootwed.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
